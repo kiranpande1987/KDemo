172cfb66a9bbecbd06ac0a71935a0947d93405b5
Kiran Pande <kiranpande1987@gmail.com>
2019-04-09 05:07:04 +0530

    Initial commit

---
+++ app/src/main/java/com/i64bits/kdemo/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.i64bits.kdemo
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
