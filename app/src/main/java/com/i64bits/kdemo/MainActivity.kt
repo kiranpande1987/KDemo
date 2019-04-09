384e78780bed66040421ca5e881df6232b71cd69
Kiran Pande <kiranpande1987@gmail.com>
2019-04-09 06:11:52 +0530

    Added DataBinding, ListView.

--- app/src/main/java/com/i64bits/kdemo/MainActivity.kt
+++ app/src/main/java/com/i64bits/kdemo/MainActivity.kt
@@ -1,12 +1,24 @@
 package com.i64bits.kdemo
 
+import android.databinding.DataBindingUtil
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ListView
+import com.i64bits.kdemo.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
+    private val listView: ListView? = null;
+
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        val binding: ActivityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        val header = Header("Kotlin Demos")
+        binding.header = header;
+        binding.executePendingBindings()
+
     }
 }
